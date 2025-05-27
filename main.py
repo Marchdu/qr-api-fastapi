1195cdf9d327516eadf0a11b8c4893fb122a9746
Marchdu <125204577+Marchdu@users.noreply.github.com>
2025-05-27 09:35:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ app = FastAPI()
 
 @app.get("/qrcode")
 def generate_qrcode(data: str = Query(...)):
-    try:
-        qr = qrcode.make(data)
-        buf = io.BytesIO()
-        qr.save(buf, format="PNG")
-        buf.seek(0)
-        return StreamingResponse(buf, media_type="image/png")
-    except Exception as e:
-        return {"error": str(e)}
+    qr = qrcode.make(data)
+    buf = io.BytesIO()
+    qr.save(buf, format="PNG")
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
